01b40747a147d94496854a6d2da81923c19da6ef
JJ_ <tub0008@naver.com>
2024-06-10 01:05:09 +0900

    feat: 스프링에서 제공해주는 포인트컷 사용 (NameMatchMethodPointcut)

--- src/test/java/hello/advanced/proxy/advisor/AdvisorTest.java
+++ src/test/java/hello/advanced/proxy/advisor/AdvisorTest.java
@@ -11,6 +11,7 @@ import org.springframework.aop.MethodMatcher;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactory;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
 
 import java.lang.reflect.Method;
 
@@ -82,5 +83,23 @@ public class AdvisorTest {
         public boolean matches(Method method, Class<?> targetClass, Object... args) {
             return false;
         }
+    }
+
+    @Test
+    @DisplayName("Spring이 제공하는 포인트컷 사용")
+    void advisorTest3() {
+        ServiceInterface target = new ServiceImpl();
+
+        // 어드바이저 생성(포인트컷, 어드바이스)
+        NameMatchMethodPointcut nameMatchMethodPointcut = new NameMatchMethodPointcut();
+        nameMatchMethodPointcut.setMappedName("save");
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(nameMatchMethodPointcut, new TimeAdvice());
+
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.search();
     }
 }
