b4e24fcc8a378068c997b1d345aca222da291a06
JJ_ <tub0008@naver.com>
2024-06-10 02:28:46 +0900

    test: BeanPostProcessor > 빈 후처리기를 통한 빈 등록 시 다른 객체로 바꿔치기해서 등록하기 테스트

--- src/test/java/hello/advanced/proxy/postprocessor/BasicTest.java
+++ src/test/java/hello/advanced/proxy/postprocessor/BasicTest.java
@@ -1,9 +1,10 @@
 package hello.advanced.proxy.postprocessor;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -36,14 +37,57 @@ public class BasicTest {
     }
 
     static class A {
+
         public void helloA() {
             log.info("helloA");
         }
     }
 
     static class B {
+
         public void helloB() {
             log.info("helloB");
         }
+    }
+
+    static class AToBPostProcessor implements BeanPostProcessor {
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            log.info("beanName={}, bean={}", beanName, bean);
+            if (bean instanceof A) {
+                return new B(); // 빈 바꿔치기
+            }
+            return bean;
+        }
+
+
+    }
+
+    @Configuration
+    static class BeanPostProcessorConfig {
+
+        @Bean(name = "beanA")
+        public A a() {
+            return new A();
+        }
+
+        @Bean
+        public AToBPostProcessor helloPostProcessor() {
+            return new AToBPostProcessor();
+        }
+
+    }
+
+    @Test
+    void postProcessorConfig() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(BeanPostProcessorConfig.class);
+
+        // B는 빈으로 등록됨
+        B b = applicationContext.getBean("beanA", B.class); // BeanPostProcessorConfig에 의해 Bean이 바꿔치기 됨
+        b.helloB();
+
+        // A는 빈으로 등록되지 않음
+        assertThrows(NoSuchBeanDefinitionException.class, () -> applicationContext.getBean(A.class));
     }
 }
